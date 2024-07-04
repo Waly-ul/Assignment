ccb5bd483acf604a4615b5741cf23887396aebb5
Waly-ul <mdwalyulamin54@gmail.com>
2024-07-04 14:31:46 +0600

    Added two decimal point in celcius_to_fahrenheit.php file

--- celsius_to_fahrenheit.php
+++ celsius_to_fahrenheit.php
@@ -3,5 +3,5 @@
 
     $temperature_in_fahrenheit = ($temperature_in_celsius * 9/5) + 32;
     
-    printf("{$temperature_in_celsius} degree celsius is equal to ".$temperature_in_fahrenheit." Fahrenheit");
+    printf("{$temperature_in_celsius} degree celsius is equal to %.2f",$temperature_in_fahrenheit." Fahrenheit");
 ?>
